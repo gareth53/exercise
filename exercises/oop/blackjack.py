5649863b8165602c6416adf3fe56618fb76a38ba
gareth53 <gareth.senior@sainsburys.co.uk>
2017-04-19 08:31:18 +0100

    blackjack - finalised solution and tests

--- exercises/oop/blackjack.py
+++ exercises/oop/blackjack.py
@@ -15,6 +15,9 @@ LABELS = {
 	13: "King"  
 }
 
+class GameError(Exception):
+	pass
+
 class Suit(Enum):
      Clubs = 1
      Hearts = 2
@@ -23,7 +26,59 @@ class Suit(Enum):
 
 
 class Game:
-	pass
+	
+	def __init__(self, players=2):
+		self.deck = Deck()
+		self.hands = [Hand(player_name="Player %d" % (x+1), game=self) for x in range(0, players)]
+
+	def _deal(self):
+		self.deck.shuffle_cards()
+		for x in range(0, 2):
+			for hand in self.hands:
+				hand.add_card(self.deck.deal_card())
+
+	def conduct_game(self):
+		self._deal()
+		for hand in self.hands:
+			while hand.completed is not True:
+				hand.stick_or_twist()
+		self._complete_game()
+
+	def _complete_game(self):
+		for score in self._get_scores():
+			print(score)
+		print(self.announce_result())
+
+	def _get_scores(self):
+		scores = []
+		for p in self.hands:
+			name = p.player_name
+			score = p.score()
+			print("{} has {}".format(name, p.describe_hand()))
+			if score == 0:
+				scores.append("{} went bust :(".format(name))
+			else:
+				scores.append("{} scored {}".format(name, score))
+		return scores
+
+	def _get_winning_hands(self):
+		# look through the hands, find highest score
+		self.hands.sort(key=lambda x: x.score())
+		winners = []
+		last_score = None
+		while self.hands:
+			hand = self.hands.pop()
+			if last_score and hand.score() < last_score:
+				break
+			last_score = hand.score()
+			winners.append(hand)
+		return winners
+
+	def _announce_result(self):
+		hands = self._get_winning_hands()
+		if len(hands) > 1:
+			return "TIE! {} all scored {}".format(", ".join([hand.player_name for hand in hands]), hands[0].score())
+		return "{} WINS with a score of {}".format(hands[0].player_name, hands[0].score())
 
 
 class Deck:
@@ -48,22 +103,33 @@ class Deck:
 
 class Hand:
 
-	def __init__(self):
+	def __init__(self, player_name=None, game=None):
 		self.cards = []
+		self.game = game
+		self.player_name = player_name
+		self.completed = False
+		self._score = None
 
 	def add_card(self, card):
+		self._score = None
 		self.cards.append(card)
+
+	def describe_hand(self):
+		cards = [card.name for card in self.cards]
+		return ", ".join(cards)
 
 	def score(self):
 		"""
 		a score of 0 means player is bust
 		"""
-		for score in self.scores():
+		if self._score:
+			return self._score
+		for score in self._scores():
 			if score <= LIMIT:
 				return score
 		return 0
 
-	def scores(self):
+	def _scores(self):
 		score = 0
 		has_ace = False
 		for card in self.cards:
@@ -74,15 +140,38 @@ class Hand:
 			return [score + 10, score]
 		return [score]
 
+	def stick(self):
+		self.completed = True
+
+	def twist(self):
+		if self.score() > 21:
+			raise BustException("A player can't twist if he's already bust!")
+		self.add_card(self.game.deck.deal_card())
+
+	def stick_or_twist(self):
+		"""
+		if any score is > 16, stick...
+		"""
+		if sorted(self._scores())[-1] > 16:
+			self.stick()
+		else:
+			self.twist()
+
 
 class Card:
 
 	@property
 	def name(self):
-		return "{} of {}".format(LABELS.get(self.value, self.value), self.suit.name)
+		return "{} of {}".format(LABELS.get(self.identifier, self.identifier), self.suit.name)
 
-	def __init__(self, suit, value):
-		if type(value) is not int:
-			raise TypeError("Card value must be an integer")
+	def __init__(self, suit, identifier):
+		if type(identifier) is not int:
+			raise TypeError("Card identifier must be an integer")
 		self.suit = suit
-		self.value = value
+		self.identifier = identifier
+		self.value = identifier if identifier < 11 else 10
+
+
+if __name__ == "__main__":
+	game = Game(players=6)
+	game.conduct_game()
--- tests/test_oop/test_blackjack.py
+++ tests/test_oop/test_blackjack.py
@@ -1,6 +1,6 @@
 import unittest
 from copy import copy
-from exercises.oop.blackjack import Card, Suit, Deck, Hand
+from exercises.oop.blackjack import Card, Suit, Deck, Hand, Game
 
 
 class TestCard(unittest.TestCase):
@@ -24,6 +24,22 @@ class TestCard(unittest.TestCase):
 		card = Card(Suit.Clubs, 13)
 		self.assertEqual(card.name, "King of Clubs")
 
+	def test_value(self):
+		card = Card(Suit.Diamonds, 1)
+		self.assertEqual(card.value, 1)
+		
+		card = Card(Suit.Hearts, 2)
+		self.assertEqual(card.value, 2)
+
+		card = Card(Suit.Hearts, 11)
+		self.assertEqual(card.value, 10)		
+
+		card = Card(Suit.Spades, 12)
+		self.assertEqual(card.value, 10)
+		
+		card = Card(Suit.Clubs, 13)
+		self.assertEqual(card.value, 10)
+
 
 class TestDeck(unittest.TestCase):
 
@@ -45,7 +61,8 @@ class TestDeck(unittest.TestCase):
 			deck.shuffle_cards()
 			self.assertNotEqual(pre_shuffle, deck.cards)
 
-class TestHand(unittest.TestCase):
+
+class TestHandScoring(unittest.TestCase):
 
 	def test_simple_score(self):
 		hand = Hand()
@@ -74,3 +91,95 @@ class TestHand(unittest.TestCase):
 		hand.add_card(Card(Suit.Hearts, 4))
 		hand.add_card(Card(Suit.Hearts, 10))
 		self.assertEqual(hand.score(), 15)
+
+
+class TestHandDescribe(unittest.TestCase):
+
+	def test_simple(self):
+		hand = Hand()
+		hand.add_card(Card(Suit.Clubs, 2))
+		hand.add_card(Card(Suit.Clubs, 10))
+		self.assertEqual(hand.describe_hand(), "2 of Clubs, 10 of Clubs")
+
+	def test_fivecard_score(self):
+		hand = Hand()
+		hand.add_card(Card(Suit.Clubs, 2))
+		hand.add_card(Card(Suit.Diamonds, 2))
+		hand.add_card(Card(Suit.Spades, 2))
+		hand.add_card(Card(Suit.Hearts, 2))
+		hand.add_card(Card(Suit.Hearts, 5))
+		self.assertEqual(hand.describe_hand(), "2 of Clubs, 2 of Diamonds, 2 of Spades, 2 of Hearts, 5 of Hearts")
+
+
+class TestHandStickOrTwist(unittest.TestCase):
+
+	def test_first_twist(self):
+		game = Game()
+		hand = Hand(game=game)
+		hand.add_card(Card(Suit.Diamonds, 2))
+		hand.add_card(Card(Suit.Hearts, 2))
+		game.hands = [hand]
+
+		hand.stick_or_twist()
+		self.assertFalse(hand.completed)
+		self.assertEqual(len(hand.cards), 3)
+
+	def test_borderline_twist(self):
+		game = Game()
+		hand = Hand(game=game)
+		hand.add_card(Card(Suit.Diamonds, 10))
+		hand.add_card(Card(Suit.Hearts, 6))
+		game.hands = [hand]
+
+		hand.stick_or_twist()
+		self.assertFalse(hand.completed)
+		self.assertEqual(len(hand.cards), 3)
+
+	def test_stick(self):
+		game = Game()
+		hand = Hand(game=game)
+		hand.add_card(Card(Suit.Diamonds, 10))
+		hand.add_card(Card(Suit.Hearts, 7))
+		game.hands = [hand]
+
+		hand.stick_or_twist()
+		self.assertTrue(hand.completed)
+
+
+class TestGame(unittest.TestCase):
+
+	def test_deal(self):
+		g = Game()
+		g._deal()
+		for hand in g.hands:
+			self.assertEqual(len(hand.cards), 2)
+			self.assertEqual(type(hand), Hand)
+
+	def test_get_scores(self):
+		g = Game()
+
+		h1 = Hand(player_name="Henry")
+		h1.add_card(Card(Suit.Diamonds, 2))
+		h1.add_card(Card(Suit.Diamonds, 3))
+		h2 = Hand(player_name="Steve")
+		h2.add_card(Card(Suit.Clubs, 10))
+		h2.add_card(Card(Suit.Spades, 11))
+		h3 = Hand(player_name="Gregg")
+		h3.add_card(Card(Suit.Spades, 10))
+		h3.add_card(Card(Suit.Diamonds, 11))
+		h3.add_card(Card(Suit.Spades, 12))
+
+		g.hands = [h1, h2, h3]
+		scores = g._get_scores()
+		self.assertEqual(len(scores), 3)
+
+		self.assertEqual(scores[0], "Henry scored 5")
+		self.assertEqual(scores[0], "Steve scored 21")
+		self.assertEqual(scores[0], "Gregg 3 went bust :(")
+
+
+	def test_get_winning_hands(self):
+		assert False
+
+	def test_announce_result(self):
+		assert False
